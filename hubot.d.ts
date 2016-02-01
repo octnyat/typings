40102480581447349c4894180cdc6ca2b71cab24
octnyat <octnyat@gmail.com>
2016-02-01 18:58:26 +0900

    add hubot typings

--- hubot.d.ts
+++ hubot.d.ts
@@ -1,25 +1,29 @@
 // Type definitions for hubot
-// Definitions by: octnyat
+// Definitions by: nyanchu
 
-declare module "hubot" {
-  export interface Robot {
-    listen(regex: RegExp, options: { [key: string]: any } | Function, callback?: (res: Response) => void): void;
-    hear(regex: RegExp, options: { [key: string]: any } | Function, callback?: (res: Response) => void): void;
-    respond(regex: RegExp, options: { [key: string]: any } | Function, callback?: (res: Response) => void): void;
-    send(user: string, ...string: string[]): void;
-    reply(user: string, ...string: string[]): void;
+declare module 'hubot' {
+  module hubot {
+    interface Robot {
+      listen(regex: RegExp, options: { [key: string]: any } | Function, callback?: (res: Response) => void): void;
+      hear(regex: RegExp, options: { [key: string]: any } | Function, callback?: (res: Response) => void): void;
+      respond(regex: RegExp, options: { [key: string]: any } | Function, callback?: (res: Response) => void): void;
+      send(user: string, ...word: string[]): void;
+      reply(user: string, ...word: string[]): void;
+    }
+
+    interface Response {
+      match: [string, string, { index: number }, { input: string } ];
+
+      send(...strings: string[]): void;
+      emote(...strings: string[]): void;
+      reply(...strings: string[]): void;
+      topic(...strings: string[]): void;
+      play(...strings: string[]): void;
+      locked(...strings: string[]): void;
+      random(items: any[]): any;
+      finish(): void;
+    }
   }
 
-  export interface Response {
-    match: [string, string, { index: number }, { input: string } ];
-
-    send(...strings: string[]): void;
-    emote(...strings: string[]): void;
-    reply(...strings: string[]): void;
-    topic(...strings: string[]): void;
-    play(...strings: string[]): void;
-    locked(...strings: string[]): void;
-    random(items: any[]): any;
-    finish(): void;
-  }
+  export = hubot;
 }
